e0d1251154b8ae2c868256ba37de7704b6e4e24e
jimmy <test@protonmail.com>
2019-04-12 22:05:23 +0300

    adding todos

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,21 @@
     <br>
     <div v-for="content in contentPreview" :key="content.title">
       <!-- <div @click="show" >{{content.body}}</div> -->
-      <h3>Title:</h3><nuxtdown-body class="body" :body="content.title" />
-      <h3>Body:</h3><nuxtdown-body class="body" :body="content.body" />
+      <h3>Title:</h3>
+      <nuxtdown-body class="body" :body="content.title" />
+      <h3>Body:</h3>
+      <nuxtdown-body class="body" :body="content.body" />
       <br>
     </div>
-
     <!-- {{contentPreview}} -->
+    
+    <!-- TODO: 
+    mint_setup vue_nuxt_nuxtdown nuxtent
+    meta_content header footer mansory_layout slideshow filters iterate_markdown style_markdown_content bulma google_fonts
+    img_descriptions git_lfs form search translate_btn
+    deploy_minifications compress_images font-awesome vuetify
+    analytics
+    domain_link robots.txt sitemap.xml ggl_search_console -->
   </div>
 </template>
 
@@ -43,7 +52,7 @@
       console.debug(contentPreview);
     },
     methods: {
-      show(){
+      show() {
         // console.log(this);
       }
     }
