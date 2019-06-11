7698f69892b0c8b1c487334b1051449801212d7d
jimmy <test@protonmail.com>
2019-06-11 04:42:33 +0300

    lazy loader settings

--- plugins/lazyload.js
+++ plugins/lazyload.js
@@ -3,8 +3,17 @@ import VueLazyload from 'vue-lazyload'
 
 // or with options
 Vue.use(VueLazyload, {
-  preLoad: 1.3,
-//   error: 'dist/error.png',
-//   loading: 'dist/loading.gif',
-  attempt: 4
+  preLoad: 1.5,
+  //   error: 'dist/error.png',
+  //   loading: 'dist/loading.gif',
+  attempt: 4,
+
+  // set observer to true
+  observer: true,
+
+  // optional
+  observerOptions: {
+    rootMargin: '5px',
+    threshold: 0.5
+  }
 })
