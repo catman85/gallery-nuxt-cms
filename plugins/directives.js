96ec967faef9997aa11dcc6c2af8db4466602e42
jimmy <test@protonmail.com>
2019-06-12 21:28:30 +0300

    adding directives.js

---
+++ plugins/directives.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+
+// You will need to add v-vpshow directive to the elements you want to animate when they become visible in the viewport.
+// check the classes  "enter" and "before-enter" in _page.scss
+Vue.directive("vpshow", {
+    inViewport(el) {
+      var rect = el.getBoundingClientRect();
+      return !(
+        rect.bottom < 0 ||
+        rect.right < 0 ||
+        rect.left > window.innerWidth ||
+        rect.top > window.innerHeight
+      );
+    },
+  
+    bind(el, binding) {
+      el.classList.add("before-enter");
+      el.$onScroll = function() {
+        if (binding.def.inViewport(el)) {
+          el.classList.add("enter");
+          el.classList.remove("before-enter");
+          binding.def.unbind(el, binding);
+        }
+      };
+      document.addEventListener("scroll", el.$onScroll);
+    },
+  
+    inserted(el) {
+      el.$onScroll();
+    },
+  
+    unbind(el) {
+      document.removeEventListener("scroll", el.$onScroll);
+      delete el.$onScroll;
+    }
+  });
