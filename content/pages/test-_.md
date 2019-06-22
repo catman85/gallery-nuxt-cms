7a2eaa3e8236325169458767ffbe9a09e6e7be2f
jimmy <jim@thinkpad.gr>
2019-06-22 21:24:04 +0300

    titles fix

--- content/pages/test-_.md
+++
@@ -1,7 +0,0 @@
----
-title: test-_/\"-'
-category: group-exhibitions
-creationDate: '2019-06-22'
-featuredImage: /images/uploads/5vcekqm.jpg
----
-asd
--- pages/index.vue
+++ pages/index.vue
@@ -125,6 +125,7 @@ robots.txt sitemap.xml ggl_search_console -->
         if (!link) return ''
         link = link.toString();
         // link = link.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, ''); // remove all special characters
+        link = link.replace(/[`!@#$%^&*()|+\=?;:'",.<>\{\}\[\]\\\/]/gi, ''); // remove all special characters except the ones netlify cms understands - _
         link = link.trim(); // remove spaces from beginning and end.
         link = link.replace(/ /g, "-"); //   / /g is a global replacement of the space character with dash
         return link.toLowerCase();
